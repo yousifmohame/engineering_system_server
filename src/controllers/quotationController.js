03e58dce0b52b8faad5ae35d76a29cf996d286af
yousif <me8999109@gmail.com>
2026-06-21 07:39:12 +0300

    تحديثات جديدة

--- src/controllers/quotationController.js
+++ src/controllers/quotationController.js
@@ -2465,7 +2465,7 @@ const generatePdfPreview = async (req, res) => {
     form.append("waitDelay", "1.5s");
 
     const response = await axios.post(
-      "http://gotenberg:3000/forms/chromium/convert/html", // 👈 تم التعديل هنا
+      "http://127.0.0.1:3000/forms/chromium/convert/html", // 👈 تم التعديل هنا
       form,
       {
         headers: { ...form.getHeaders() },
