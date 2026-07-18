5e1cdcc7903aaaf8c5da23a7a3a939573ad814d1
yousif <me8999109@gmail.com>
2026-07-18 09:20:12 +0300

    تحديثات جديدة

--- src/routes/privateTransactionRoutes.js
+++ src/routes/privateTransactionRoutes.js
@@ -114,8 +114,8 @@ router.post(
 router.post("/:id/payments", addPayment);
 router.delete("/:id/payments/:paymentId", deletePayment);
 
-router.post("/:id/expenses", protect, addExpense);
-router.delete("/:id/expenses/:expenseId", protect, deleteExpense);
+router.post("/:id/tx-expenses", protect, addExpense);
+router.delete("/:id/tx-expenses/:expenseId", protect, deleteExpense);
 
 router.post("/:id/collection-dates", addCollectionDate);
 router.delete("/:id/collection-dates/:dateId", deleteCollectionDate);
